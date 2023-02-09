a7baec204ddd9824058800665f22423cfbfec40c
Rene Mark Barrow <72480653+rb-creator@users.noreply.github.com>
2023-02-09 17:20:33 +0000

    AI and Navmesh implemented

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,9 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    public Transform PatrolRoute;
+    public List<Transform> Locations;
+    private int _locationIndex = 0;
+    private NavMeshAgent _agent;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    private void Update()
+    {
+        if (_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    private void InitializePatrolRoute()
+    {
+        foreach (Transform child in PatrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+
+    private void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+            return;
+        _agent.destination = Locations[_locationIndex].position;
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.name == "Player")
